323a0c8714219eebdff21e1008128a07fda5824a
Marufa Akter <marufaakter1947@gmail.com>
2026-01-28 10:43:20 +0600

    Added useRole Hook for role wise response

---
+++ src/hooks/useRole.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useRole = (email) => {
+  const [role, setRole] = useState(null);
+
+  useEffect(() => {
+    if (!email) return;
+
+    axios
+      .get(`http://localhost:5000/users/role?email=${email}`)
+      .then((res) => {
+        // from backend response
+        if (res.data.role) setRole(res.data.role);
+      })
+      .catch((err) => console.error("Error fetching role:", err));
+  }, [email]);
+
+  return role;
+};
+
+export default useRole;
